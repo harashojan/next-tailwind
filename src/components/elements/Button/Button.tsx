ab45de0fc42538e53e189ee4a6c439dca876c954
harashojan <harashojan@gmail.com>
2022-07-21 14:31:18 +0900

    import React 変更

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -67,25 +67,3 @@ export const Button = forwardRef<Element, ButtonProps<Tag>>((props, ref) => {
 });
 
 Button.displayName = "Button";
-
-// import { cloneElement, forwardRef } from "react";
-
-// import { Button as MantineButton } from "@mantine/core";
-
-// import type { ButtonProps } from "@mantine/core";
-
-// export const Button = forwardRef<
-//   HTMLButtonElement,
-//   ButtonProps<"button" | "a"> & { dent?: boolean }
-// >(({ sx, dent, ...props }, ref) => {
-//   return cloneElement(<MantineButton />, {
-//     sx: {
-//       ...sx,
-//       "&:not(:disabled):active": dent ? undefined : { transform: "none" },
-//     },
-//     ref,
-//     ...props,
-//   });
-// });
-
-// Button.displayName = "Button";
